b338f3f6d1adf4ec538a8cd3110f443bb70a5df2
Mircea-12 <mircea.herlas@student.upt.ro>
2024-12-28 02:48:27 +0200

    Constructori si Mosteniri Discipline si Note

--- CarnetDeNote/Disciplina.cs
+++ CarnetDeNote/Disciplina.cs
@@ -1,13 +1,24 @@
-namespace CarnetDeNote;
-
 public abstract class Disciplina
 {
-    private string Nume;
-    private int Semestru;
-    private int An;
+    protected string Nume { get; set; }
+    protected int Semestru { get; set; }
+    protected int An { get; set; }
 
     public Disciplina(string nume, int semestru, int an)
     {
+        if (string.IsNullOrWhiteSpace(nume))
+        {
+            throw new ArgumentException("Numele disciplinei nu poate fi gol.");
+        }
+        if (semestru < 1 || semestru > 2)
+        {
+            throw new ArgumentException("Semestrul trebuie să fie 1 sau 2.");
+        }
+        if (an < 1)
+        {
+            throw new ArgumentException("Anul trebuie să fie cel puțin 1.");
+        }
+
         Nume = nume;
         Semestru = semestru;
         An = an;
---
+++ CarnetDeNote/DisciplinaObligatorie.cs
@@ -0,0 +1,8 @@
+namespace CarnetDeNote;
+
+public class DisciplinaObligatorie:Disciplina
+{
+    public DisciplinaObligatorie(string nume, int semestru, int an) : base(nume, semestru, an)
+    {
+    }
+}
